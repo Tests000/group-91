c76bec9bdf0ca40446afd2a98a797f58a36169e2
илья клюев <ilya-klyuev-2002@mail.ru>
2022-09-14 11:08:14 +0300

    change lab1 ilya klyuev

--- ilya.klyuev/lab1/src/CorrectInput.java
+++ ilya.klyuev/lab1/src/CorrectInput.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class CorrectInput {
-    private int InputInt() {
+    private int inputInt() {
         int number = 0;
         boolean isCorrectInput = false;
         Scanner scanner = new Scanner(System.in);
@@ -19,14 +19,14 @@ public class CorrectInput {
         return number;
     }
 
-    public int InputIntInRange(String message, int leftBound, int rightBound) {
+    public int inputIntInRange(String message, int leftBound, int rightBound) {
         int number = 0;
         boolean isCorrectInput = false;
 
         while (!isCorrectInput) {
             System.out.println(message);
             System.out.printf("Введите число от %d до %d\n", leftBound, rightBound);
-            number = InputInt();
+            number = inputInt();
             isCorrectInput = number >= leftBound && number <= rightBound;
             if (!isCorrectInput) {
                 System.out.println("Число не входит в заданный диапазон. Повторите попытку");
@@ -36,12 +36,12 @@ public class CorrectInput {
     }
 
     public int[] inputTemperatures() {
-        int temperaturesNumber = InputIntInRange("Введите количество дней", 1, 105);
+        int temperaturesNumber = inputIntInRange("Введите количество дней", 1, 105);
         int[] temperatures = new int[temperaturesNumber];
 
         for (int i = 0; i < temperaturesNumber; i++) {
             String message = String.format("Введите температуру за %d день", i + 1);
-            temperatures[i] = InputIntInRange(message, 30, 100);
+            temperatures[i] = inputIntInRange(message, 30, 100);
         }
         return temperatures;
     }
