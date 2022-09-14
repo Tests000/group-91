689e0407c3be382e7043f1b6596bc00b0d2bf54d
YakuninAlexander <wepqwppw@gmail.com>
2022-09-14 09:48:52 +0300

    add lab1

---
+++ alexander.yakunin/lab1/src/Program.java
@@ -0,0 +1,9 @@
+public class Program {
+    public static void main(String[] args) {
+        Tournament t = new Tournament();
+        // Я конечно не уверен, что по моему можно выводить countOfCommands - 1 ???
+        // Но для этого нужно математически доказывать, а мне лень )))))
+
+        System.out.println(t.calculateGames());
+    }
+}
---
+++ alexander.yakunin/lab1/src/Tournament.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Tournament {
+    private int getCountOfCommands() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input count of commands");
+        int result = in.nextInt();
+        while (result<=0) {
+            System.out.println("Input error! Count of commands must be positive number");
+            result = in.nextInt();
+        }
+
+        return result;
+    }
+
+    public int calculateGames() {
+        int countOfCommands = getCountOfCommands(); // Обработка ввода кол-ва команд
+        int countOfGames = 0;
+
+        while(countOfCommands != 1) {
+            countOfGames += countOfCommands / 2;
+            countOfCommands = countOfCommands % 2 == 0 ? countOfCommands / 2 : countOfCommands / 2 + 1;
+        }
+
+        return countOfGames;
+    }
+}
+
+
