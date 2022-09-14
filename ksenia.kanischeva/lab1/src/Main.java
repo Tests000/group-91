3cac1871ba6e7eb1dfce4e138169d9c39b0bad06
Ксения Канищева <ksenia.kanishewa@yandex.ru>
2022-09-14 11:18:03 +0300

    Unfinished lab

--- ksenia.kanischeva/lab1/src/Main.java
+++ ksenia.kanischeva/lab1/src/Main.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите слова");
+
+        String line = in.nextLine();
+
+
+    }
+
+//    private bool isCorrect(String[] words){
+//
+//    }
 }
--- ksenia.kanischeva/lab1/src/UniqueMorseCodeWords.java
+++ ksenia.kanischeva/lab1/src/UniqueMorseCodeWords.java
@@ -1,6 +1,43 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class UniqueMorseCodeWords {
     private static final String[] COODES = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.",
             "---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."} ;
 
+    private String[] transform(String[] words){
+        if (words.length == 0){
+            return null;
+        }
+
+
+        String[] transformedWords = new String[words.length];
+
+        for (int i = 0; i < words.length; i++){
+            String word = words[i];
+            StringBuilder transformedWord = new StringBuilder();
+            for (int j = 0; j < word.length(); j++){
+                transformedWord.append(COODES[word.charAt(j) - 'a']);
+            }
+            transformedWords[i] = transformedWord.toString();
+        }
+
+        return transformedWords;
+    }
+
+    private int countUnigueCodes(String[] transformedWords){
+        if (transformedWords == null){
+            return 0;
+        }
+
+        Set<String> uniqueWords = new HashSet<>(Arrays.asList(transformedWords));
+        return uniqueWords.size();
+    }
+
+//    public int countUniqueWords(String){
+//        return countUnigueCodes(transform())
+//
+//    }
 
 }
