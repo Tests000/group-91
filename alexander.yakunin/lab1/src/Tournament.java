828bd8ca5677eed53275a34d0868b43e1608a119
YakuninAlexander <wepqwppw@gmail.com>
2022-09-14 10:06:40 +0300

    add lab1 in Java style ?_?

--- alexander.yakunin/lab1/src/Tournament.java
+++ alexander.yakunin/lab1/src/Tournament.java
@@ -5,6 +5,7 @@ public class Tournament {
         Scanner in = new Scanner(System.in);
         System.out.println("Input count of commands");
         int result = in.nextInt();
+
         while (result<=0) {
             System.out.println("Input error! Count of commands must be positive number");
             result = in.nextInt();
@@ -19,7 +20,9 @@ public class Tournament {
 
         while(countOfCommands != 1) {
             countOfGames += countOfCommands / 2;
-            countOfCommands = countOfCommands % 2 == 0 ? countOfCommands / 2 : countOfCommands / 2 + 1;
+            countOfCommands = (countOfCommands % 2 == 0) ?
+                    countOfCommands / 2 :
+                    countOfCommands / 2 + 1;
         }
 
         return countOfGames;
