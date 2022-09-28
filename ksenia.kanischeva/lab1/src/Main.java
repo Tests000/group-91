f2ad458eb3e84341d870173ed214b9e9c23c7435
MrsSbr <daria.sanna@gmail.com>
2022-09-28 08:51:33 +0300

    added second task

--- ksenia.kanischeva/lab1/src/Main.java
+++ ksenia.kanischeva/lab1/src/Main.java
@@ -1,24 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
         String line;
         UniqueMorseCodeWords uniqueMorseCodeWords = new UniqueMorseCodeWords();
 
-        while (true){
+        while (true) {
             System.out.print("Input: ");
             line = in.nextLine();
-            if (line.equals("")){
+            if (line.equals("")) {
                 break;
             }
             System.out.print("Output: ");
             try {
                 int count = uniqueMorseCodeWords.getCountUniqueWords(line);
                 System.out.println(count);
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
--- ksenia.kanischeva/lab1/src/UniqueMorseCodeWords.java
+++ ksenia.kanischeva/lab1/src/UniqueMorseCodeWords.java
@@ -4,18 +4,18 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 public class UniqueMorseCodeWords {
-    private static final String[] COODES = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.",
-            "---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."} ;
+    private static final String[] COODES = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.",
+            "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
 
     private String[] transform(String[] words) throws Exception {
         String[] transformedWords = new String[words.length];
 
-        for (int i = 0; i < words.length; i++){
+        for (int i = 0; i < words.length; i++) {
             String word = words[i];
             StringBuilder transformedWord = new StringBuilder();
-            for (int j = 0; j < word.length(); j++){
+            for (int j = 0; j < word.length(); j++) {
                 int position = word.charAt(j) - 'a';
-                if (position < 0 || position >= COODES.length){
+                if (position < 0 || position >= COODES.length) {
                     throw new Exception("Incorrect word format " + word);
                 }
                 transformedWord.append(COODES[position]);
@@ -26,27 +26,27 @@ public class UniqueMorseCodeWords {
         return transformedWords;
     }
 
-    private int countUniqueCodes(String[] transformedWords){
+    private int countUniqueCodes(String[] transformedWords) {
         Set<String> uniqueWords = new HashSet<>(Arrays.asList(transformedWords));
         return uniqueWords.size();
     }
 
     public int getCountUniqueWords(String line) throws Exception {
         String[] words = line.split(", ");
-        if (isCorrect(words)){
+        if (isCorrect(words)) {
             return countUniqueCodes(transform(words));
         }
         return 0;
     }
 
     private boolean isCorrect(String[] words) throws Exception {
-        if (words.length == 0 || words.length > 100){
+        if (words.length == 0 || words.length > 100) {
             throw new Exception("Incorrect number of words");
         }
 
         final Stream<String> longWords = Arrays.stream(words).filter(word -> word.length() == 0 || word.length() > 12);
 
-        if (longWords.count() > 0){
+        if (longWords.count() > 0) {
             throw new Exception("Incorrect length of words " + longWords);
         }
 
