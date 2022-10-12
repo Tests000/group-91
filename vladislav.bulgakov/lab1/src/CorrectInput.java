1e6367d1d667d4c01a9f067d9379806560b86769
MrsSbr <daria.sanna@gmail.com>
2022-10-12 10:07:59 +0300

    fixed and second tasks

--- vladislav.bulgakov/lab1/src/CorrectInput.java
+++ vladislav.bulgakov/lab1/src/CorrectInput.java
@@ -2,16 +2,15 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CorrectInput {
-    private static int getInt(){
+    private static int getInt() {
         boolean isCorrect = false;
         int intNumber = 0;
         Scanner scanner = new Scanner(System.in);
-        while(!isCorrect){
+        while (!isCorrect) {
             try {
                 intNumber = scanner.nextInt();
                 isCorrect = true;
-            }
-            catch (InputMismatchException ime) {
+            } catch (InputMismatchException ime) {
                 System.out.println("Вводимое значение должно быть целым числом");
                 ime.printStackTrace();
                 scanner.nextLine();
@@ -20,23 +19,23 @@ public class CorrectInput {
         return intNumber;
     }
 
-    public static int getIntInRange(int minValue, int maxValue){
+    public static int getIntInRange(int minValue, int maxValue) {
         boolean isCorrect = false;
         int intNumber = 0;
-        while(!isCorrect){
+        while (!isCorrect) {
             System.out.println("Введите число в диапазоне от " + minValue + " до " + maxValue);
             intNumber = getInt();
             isCorrect = intNumber >= minValue && intNumber <= maxValue;
-            if (!isCorrect){
+            if (!isCorrect) {
                 System.out.println("Число вне диапазона");
             }
         }
         return intNumber;
     }
 
-    public static int[] getIntArray(int size){
+    public static int[] getIntArray(int size) {
         int[] array = new int[size];
-        for(int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
             array[i] = getIntInRange(0, 1000);
         }
         return array;
--- vladislav.bulgakov/lab1/src/JumpGame.java
+++ vladislav.bulgakov/lab1/src/JumpGame.java
@@ -1,50 +1,49 @@
 public class JumpGame {
 
     public static void printArray(int[] array) {
-        for(int i : array){
+        for (int i : array) {
             System.out.print(i + " ");
         }
         System.out.println();
     }
 
-    public static int getMinimumJumps(int[]array, int size){
-        int[] jumps = new int [size];
+    public static int getMinimumJumps(int[] array, int size) {
+        int[] jumps = new int[size];
         int count = 0;
         int jumpsCount = size;
         int index = 0;
 
         jumps[0] = array[index];
-        while(jumps[0] != 0) {
-            boolean isNull = false;
+        while (jumps[0] != 0) {
+            boolean isZeroValue = false;
 
-            while(index < size - 1 && !isNull){
+            while (index < size - 1 && !isZeroValue) {
                 int newPos = index + jumps[count];
-                if(newPos > size - 1){
+                if (newPos > size - 1) {
                     jumps[count] = size - 1 - index;
                     index = size - 1;
                     count++;
                     jumps[count] = array[index];
-                }
-                else {
+                } else {
                     if (index < size - 1 && array[newPos] != 0 || newPos == size - 1) {
                         index += jumps[count];
                         count++;
                         jumps[count] = array[index];
                     }
                     if (index < size - 1 && array[newPos] == 0) {
-                        isNull = true;
+                        isZeroValue = true;
                     }
                 }
             }
 
-            if(count < jumpsCount && !isNull){
+            if (count < jumpsCount && !isZeroValue) {
                 jumpsCount = count;
             }
 
-            if(jumps[count] != 0){
+            if (jumps[count] != 0) {
                 jumps[count]--;
             }
-            while(index > 0 && jumps[count] == 0){
+            while (index > 0 && jumps[count] == 0) {
                 count--;
                 index -= jumps[count];
                 jumps[count]--;
