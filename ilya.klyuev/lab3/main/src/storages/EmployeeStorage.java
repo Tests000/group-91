0f3d368419b8b79b99b4aed560e3c3755ffb1c32
klyuevskiy <ilya-klyuev-2002@mail.ru>
2022-11-02 07:42:30 +0300

    change lab3

---
+++ ilya.klyuev/lab3/main/src/storages/EmployeeStorage.java
@@ -0,0 +1,37 @@
+package storages;
+
+import factories.ListFactory;
+import models.Employee;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class EmployeeStorage {
+    private final List<Employee> employees;
+    private final Logger logger;
+
+    private final int MAX_EMPLOYEES_COUNT = 12000;
+
+    public EmployeeStorage(Logger logger){
+        this.logger = logger;
+        employees = ListFactory.createList();
+    }
+
+    public void addEmployees(Collection<Employee> employees){
+        if (employees.size() + this.employees.size() <= MAX_EMPLOYEES_COUNT){
+            this.employees.addAll(employees);
+        } else{
+            logger.log(Level.WARNING, "Превышено максимальное количество работников");
+        }
+    }
+
+    public int getEmployeesCount(){
+        return employees.size();
+    }
+
+    public int getAllowedEmployeesCount(){
+        return MAX_EMPLOYEES_COUNT - getEmployeesCount();
+    }
+}
